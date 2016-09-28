7d20c4c5f5f6324e9835bdfd5b1329feb3b11c54
Nicolas Iderhoff <nicolasiderhoff@googlemail.com>
2016-09-28 18:04:37 +0200

    added vis submodule

---
+++ vis/__init__.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from astropy.table import Table
+
+
+def plot_history(history):
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def write_history(history, outfile):
+    log1 = Tee(outfile, 'w')
+    print(Table(history.history))
+    log1.__del__()
