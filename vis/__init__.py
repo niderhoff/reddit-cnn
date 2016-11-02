8221f2e4dd72d33212ad2ee32a49b99f7955b27d
Nicolas Iderhoff <nicolasiderhoff@googlemail.com>
2016-11-02 17:32:46 +0100

    added command for ROC and AUC

--- vis/__init__.py
+++ vis/__init__.py
@@ -2,6 +2,7 @@ import sys
 import matplotlib.pyplot as plt
 from astropy.table import Table
 from keras.utils.visualize_util import plot
+from sklearn.metrics import roc_curve, auc
 import numpy as np
 
 
@@ -64,3 +65,19 @@ def plot_nn_graph(nnobj, to_file=False,
                   show_shapes=False, show_layer_names=False):
     plot(nnobj, to_file=to_file, show_shapes=show_shapes,
          show_layer_names=show_layer_names)
+
+
+def plot_roc(model, X_test, y_test, y_score):
+    y_score = model.predict(X_test)
+    fpr, tpr, _ = roc_curve(y_test, y_score)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.05])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic curve')
+    plt.show()
+    print('AUC: %f' % roc_auc)
